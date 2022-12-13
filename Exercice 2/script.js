e8dc2bbc420c93779d2b1142cdf36cf1daeb49bf
Virginie Dourson <virginiedourson@gmail.com>
2022-12-13 13:56:47 +0100

    Exercice 2

--- Exercice 2/script.js
+++ Exercice 2/script.js
@@ -1,6 +1,17 @@
-let naissance = new Date(1977,7,11);
+/*let naissance = new Date(1977,7,11);
 let init = naissance.getTime();
 let today = new Date();
-let final = today.getTime()
+let final = today.getTime();
 let days = Math.floor((final-init)/86400000);
-document.getElementById("h").innerHTML = "Le nombre de jours écoulés depuis ma naissance est de : " + days;
+document.getElementById("h").innerHTML = "Le nombre de jours écoulés depuis ma naissance est de : " + days;*/
+
+function tempsEcoule(annee){
+    let init = annee.getTime();
+    let today = new Date();
+    let final = today.getTime();
+    let days = Math.floor((final-init)/86400000);
+    document.getElementById("h").innerHTML = "Le nombre de jours écoulés depuis le " + annee.toLocaleDateString() + " est de " + days + " jours";
+}
+
+let naissance = new Date(1977,7,11);
+tempsEcoule(naissance);
